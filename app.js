eeea65d8171abc7d9328451a1d231ab5600b68a8
MS Hasan <sumonhasan.md111@gmail.com>
2021-12-13 17:56:12 +0600

    add initial file

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const req = require('express/lib/request')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Hi')
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING IS PORT ${PORT}`)
+})
